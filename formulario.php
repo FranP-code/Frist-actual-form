b9a134ebd478fb14d826dfdfd66918a144d08717
Francisco P <franpessano1@gmail.com>
2021-04-30 13:45:15 -0300

    Proyecto formulario concluido pero no terminado

--- formulario.php
+++ formulario.php
@@ -1,35 +1,32 @@
 <?php
 
-$error = ['Name not placed', 'Email not placed', 'Message not placed', 'Bad word founded in '];
-
+$error = ['Name not placed', 'Email not valid', 'Message not placed', 'Bad word founded in '];
+$name = null;
+$email = null;
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['electronic-mail'];
-
+}
     if (!empty($name)) {
+        $name = trim($name);
         $name = filter_var($name, FILTER_SANITIZE_STRING);
-
-    } if (!empty($name)) {
-        // if (badWords($name) == true) {
-        //     echo $error[3] . 'name';
-
-        // } else {
-        //     echo $name;
-        // }
-
-    }
 
     if (!empty($email)) {
         $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-
-
-        }
         
     }
 }
+
+// } if (!empty($name)) {
+//     // if (badWords($name) == true) {
+//     //     echo $error[3] . 'name';
+
+//     // } else {
+//     //     echo $name;
+//     // }
+
+// }
 
 // function badWords ($v) {
 //     $badWords = ['fuck', 'prick', 'bastard', 'bellend', 'ass', 'cunt', 'balls', 'shit'];
@@ -62,12 +59,27 @@ if (isset($_POST['submit'])) {
 <body>
     <div class="form-container">
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="form">
-            <input type="text" placeholder="Name" name="name" class="name">
-            <input type="email" placeholder="E-mail" name="electronic-mail" class="email">
-            <textarea placeholder="Message" name="message" minlength="35" class="message"></textarea>
-            <input type="submit" value="Send" name="submit" class="submit">
+            <input type="text" placeholder="Name" name="name" class="name" id="name">
+            <input type="text" placeholder="E-mail" name="electronic-mail" class="email" id="email">
+            <textarea placeholder="Message" name="message" minlength="35" class="message" id="message"></textarea>
+            <input type="submit" value="Send" name="submit" class="submit" id="submit">
         </form>
     </div>
-    <h2><?php echo 'Tu nombre es: ' . $name;?></h2>
+    <?php if ($name != null) { if(!empty($name)) {
+        
+        echo '<div class="error-name"><h2 class="error-name">Tu nombre es: ' . $name . '</div></h2>';
+        
+        } }?>
+    
+    <h2 class="error-email"><?php
+    if ($email != null) {
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            echo '<div class="error-email">Your email is: ' . $email . '</div>';
+
+        } else {
+            echo 'Email not valid';
+        }
+    }
+    ?></h2>
 </body>
 </html>
